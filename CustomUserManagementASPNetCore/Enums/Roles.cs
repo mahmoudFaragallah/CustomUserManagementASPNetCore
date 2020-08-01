5bcf15f31fac9f65a339e8cc6b26ca7ddf793c88
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-08-01 03:39:57 +0200

    Add project files.

---
+++ CustomUserManagementASPNetCore/Enums/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomUserManagementASPNetCore.Enums
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
